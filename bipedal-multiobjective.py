406c7eb78bad6f363a5ccf2f915e054e1f58c3ff
Marcelo Dias Avelino <valorcurse@gmail.com>
2019-11-10 17:18:57 +0100

    Changes to accomodate changs in neat.

--- bipedal-multiobjective.py
+++ bipedal-multiobjective.py
@@ -32,8 +32,8 @@ env_name = "BipedalWalker-v2"
 env = gym.make(env_name)
 nproc = 4
 
-envs_size = 10
-pop_size = 10
+envs_size = 50
+pop_size = 100
 max_stagnation = 25
 encoding_dim = 8
 behavior_dimensions = 7
@@ -139,16 +139,6 @@ class TestOrganism(Evaluation):
 
         return (fitnesses, flattened_states)
 
-# def chunk_testing(phenotypes):
-#     all_states = []
-#     fitnesses = []
-#     for chunk in chunks(phenotypes, envs_size):
-#         fitness, states = test_organism(chunk, envs)
-#         all_states.extend(states)
-#         fitnesses.extend(fitness)
-#
-#     return (np.array(all_states), np.array(fitnesses))
-
 def run_env_once(phenotype):
     # single_envs = SubprocVecEnv([make_env(env_name, envs_size)])
     # test_organism([phenotype], single_envs, render=True)
@@ -179,33 +169,6 @@ def run_env_once(phenotype):
     # env.close()
     # Visualize().close()
 
-# def refine_ae(autoencoder, phenotypes):
-#     autoencoder.load_weights('basic.h5')
-# 
-#     last_loss = 1000.0
-#     loss = 1000.0
-# 
-#     while loss <= last_loss:
-#         last_loss = loss
-# 
-#         states, _ = chunk_testing(phenotypes)
-# 
-#         ten_percent = max(1, int(states.shape[0] * 0.1))
-# 
-#         train = states[:-ten_percent]
-#         test = states[-ten_percent:]
-# 
-#         hist = autoencoder.fit(train, train,
-#                                epochs=50,
-#                                batch_size=256,
-#                                shuffle=True,
-#                                validation_data=(test, test),
-#                                verbose=0)
-# 
-#         loss = abs(hist.history['val_loss'][-1])
-# 
-#         print("Training autoencoder. Loss: {}".format(loss))
-
 def pad_matrix(all_states, matrix_width):
     padded = []
     for row in all_states:
@@ -216,61 +179,14 @@ def pad_matrix(all_states, matrix_width):
 
     return np.array(padded)
 
-# def chunks(l, n):
-#     """Yield successive n-sized chunks from l."""
-#     for i in range(0, len(l), n):
-#         yield l[i:i + n]
-
 if __name__ == '__main__':
 
-    # import keras
-    # from keras.layers import Input, Dense
-    # from keras.models import Model
-    # from keras.backend.tensorflow_backend import set_session
-    # import tensorflow as tf
-    #
-    # config = tf.ConfigProto()
-    # config.gpu_options.allow_growth = True  # dynamically grow the memory used on the GPU
-    # config.log_device_placement = True  # to log device placement (on which device the operation ran)
-    # sess = tf.Session(config=config)
-    # set_session(sess)  # set this TensorFlow session as the default session for Keras
-
     sys.setrecursionlimit(10000)
-
-    # env = gym.make(env_name)
 
     inputs = env.observation_space.shape[0]
     outputs = env.action_space.shape[0]
 
     print("Inputs: {} | Outputs: {}".format(inputs, outputs))
-
-    # novelty_map = NoveltySearch(encoding_dim)
-
-    ############################################## Auto encoder ##############################################
-
-    # # this is our input placeholder
-    # input_img = Input(shape=(behavior_matrix_size,))
-    # # "encoded" is the encoded representation of the input
-    # encoded = Dense(encoding_dim, activation='sigmoid')(input_img)
-    # # "decoded" is the lossy reconstruction of the input
-    # decoded = Dense(behavior_matrix_size, activation='relu')(encoded)
-    #
-    # # this model maps an input to its reconstruction
-    # autoencoder = Model(input_img, decoded)
-    #
-    # # this model maps an input to its encoded representation
-    # encoder = Model(input_img, encoded)
-    #
-    # # create a placeholder for an encoded (32-dimensional) input
-    # encoded_input = Input(shape=(encoding_dim,))
-    # # retrieve the last layer of the autoencoder model
-    # decoder_layer = autoencoder.layers[-1]
-    # # create the decoder model
-    # decoder = Model(encoded_input, decoder_layer(encoded_input))
-    #
-    # autoencoder.compile(optimizer='adam', loss='binary_crossentropy')
-    # autoencoder.save_weights('basic.h5')
-    ############################################################################################################
 
     print("Creating hyperneat object")
     # pop_config = MapElitesConfiguration(4, pop_size, encoding_dim, inputs, outputs)
@@ -280,84 +196,25 @@ if __name__ == '__main__':
 
     start_fitness = np.zeros(pop_size)
     start_features = np.zeros((pop_size, encoding_dim))
-    # phenotypes: List[Phenotype] = hyperneat.epoch(MapElitesUpdate(start_fitness, start_features))
-    # phenotypes: List[Phenotype] = neat.epoch(SpeciesUpdate(start_fitness))
 
-    # highest_fitness: float = -1000.0
-    # highest_novelty: float = 0.0
-    # highestDistance: float = 0.0
-
-
-    # envs = [make_env(env_name, seed) for seed in range(envs_size)]
-    #
-    # print("Creating envs...")
-    # envs = SubprocVecEnv(envs)
-    # print("Done.")
-
-    # loss = 1000.0
-    # progress_stagnation = 0
-    # train_ae = progress_stagnation == max_stagnation
-
-    # refine_ae(autoencoder, phenotypes)
-
-
-    # epoch_num = 0
-    # while True:
-    #     epoch_num += 1
-    #     progress_stagnation += 1
-    #     train_ae = progress_stagnation == max_stagnation
-    #
-    #     if train_ae:
-    #         ae_phenotypes = []
-    #         if len(neat.population) > 0:
-    #
-    #             sorted_archive = sorted(neat.population.population_and_fitnesses(), key=lambda a: a['fitness'])
-    #             ae_phenotypes = [g['genome'].createPhenotype() for g in sorted_archive]
-    #         else:
-    #             ae_phenotypes = phenotypes
-    #
-    #         refine_ae(autoencoder, ae_phenotypes)
-    #         ten_percent = max(1, int(len(ae_phenotypes) * 0.1))
-    #         # phenotypes = ae_phenotypes[:ten_percent]
-    #
-    #         # Re-evaluate all the genomes in the archives
-    #         ae_states, _ = chunk_testing(ae_phenotypes[:ten_percent])
-    #         ae_pred = encoder.predict(ae_states)
-    #
-    #         novelty_map.reset()
-    #         novelty_map.calculate_novelty(ae_pred)
-    #
-    #         neat.population.archive = {}
-    #         progress_stagnation = 0
-
-
-
-        # Test the phenotypes in the envs
-        # start = time.time()
-        # all_states, fitnesses = chunk_testing(phenotypes)
-        # end = time.time()
-        # print("Time:", end - start)
-        #
-        #
-        # pred = encoder.predict(all_states)
-        # novelties = novelty_map.calculate_novelty(pred)
-
-        # ndf, dl, dc, ndr = pg.fast_non_dominated_sorting(points=zip(fitnesses, novelties))
-
-        # print("Highest fitness this epoch:", max(fitnesses))
-
+    highest_fitness = -1000.0
 
     for _ in neat.epoch():
 
+        max_fitness =  max([(g.fitness, g) for g in neat.population.genomes], key=lambda e: e[0])
+
         # max_fitness = max(zip(fitnesses, phenotypes), key=lambda e: e[0])
         # max_novelty = max(zip(novelties, phenotypes), key=lambda e: e[0])
-        #
-        # if max_fitness[0] > highest_fitness or max_novelty[0] > highest_novelty:
+
+        print(max_fitness[0], highest_fitness)
+
+        if max_fitness[0] > highest_fitness:
         #     highest_fitness = max_fitness[0] if max_fitness[0] > highest_fitness else highest_fitness
         #     highest_novelty = max_fitness[0] if max_fitness[0] > highest_fitness else highest_novelty
         #
         #     best_phenotype = max_fitness[1]
-        #     run_env_once(best_phenotype)
+            run_env_once(max_fitness[1].createPhenotype())
+            highest_fitness = max_fitness[0]
 
         print("########## Epoch {} ##########".format(neat.epochs))
         # print("Highest fitness all-time: {}".format(highest_fitness))
@@ -390,9 +247,5 @@ if __name__ == '__main__':
                 s.numToSpawn])
 
         print(table)
-
-        # phenotypes = hyperneat.epoch(SpeciesUpdate(fitnesses))
-        # phenotypes = hyperneat.epoch(SpeciesUpdate(novelties))
-        # phenotypes = hyperneat.epoch(MapElitesUpdate(fitnesses, pred))
 
     env.close()
